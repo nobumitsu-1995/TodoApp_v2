0dbc2c7c9d9e3be159f57dc01a7727b9b102a89f
masugata1995 <masugata1995@gmail.com>
2021-12-16 21:33:10 +0900

    S3設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,7 +29,6 @@ class UsersController < ApplicationController
   # PATCH/PUT /users/1 or /users/1.json
   def update
     if @user.update(user_params)
-      @user.image.attach(params[:user][:image])
       redirect_to @user, notice: "ユーザー情報を更新しました。"
     else
       render :edit, status: :unprocessable_entity
