ced2a0fbc7b152d93f6877ff2778dca046a5479c
masugata1995 <masugata1995@gmail.com>
2021-12-20 18:14:31 +0900

    edit user_avatar

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,9 +1,3 @@
 module UsersHelper
-    def user_icon(size)
-        if current_user.image
-            image_tag current_user.image, style: "#{image_style(size)}"
-        else
-            image_tag "/images/user_icon.png", style: "#{image_style(size)}"
-        end
-    end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,6 @@ class User < ApplicationRecord
   validates :name, length: { maximum: 20 }, presence: true
   validates :email, length: { maximum: 50 }, presence: true, uniqueness: true
   validates :password, length: { maximum: 20 }, presence: true
-
+  validates :image, presence: true
 
 end
