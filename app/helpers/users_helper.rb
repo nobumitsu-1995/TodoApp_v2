73e695543846c156b8dfbfc9f33320ba98bf6177
masugata1995 <masugata1995@gmail.com>
2021-12-20 17:08:10 +0900

    edit user_avatar

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,6 +1,6 @@
 module UsersHelper
     def user_icon(size)
-        if !@current_user.image
+        if @current_user.image
             image_tag @current_user.image, style: "#{image_style(size)}"
         else
             image_tag "/images/user_icon.png", style: "#{image_style(size)}"
